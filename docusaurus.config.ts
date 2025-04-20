e59595c129c7b6216b54dd07f5ed298f7259927e
Isfhan Ahmed <isfhan729@gmail.com>
2025-04-20 22:20:38 +0500

    chore: update edit URL in Docusaurus configuration
    - Modified the edit URL in the Docusaurus config to point to the main repository directory for better accessibility.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/isfhan/burger-api-website/tree/main/docs",
+            "https://github.com/isfhan/burger-api-website/tree/main/",
         },
         // blog: {
         //   showReadingTime: true,
